e3b934e1ef26cc878f2c1ec9235632ca092b023e
Nay Thu Naing <softguide.khaingzarlynn@gmail.com>
2023-12-28 13:09:33 +0630

    done profile

--- resources/views/layouts/Backend/Employee/employeeDetail.blade.php
+++ resources/views/layouts/Backend/Employee/employeeDetail.blade.php
@@ -11,101 +11,50 @@
               <div class="card mb-4">
                 <h5 class="card-header"></h5> 
                  <!-- Account -->
-                 <div class="card-body">
-                    <div class=" gap-4">
-                      @if(isset($employee))
-                          <img
-                          src="{{$employee->profilePath()}}"
-                          alt="user-avatar"
-                          class="d-block rounded-circle border border-info p-1"
-                          height="140"
-                          width="140"
-                          id="uploadedAvatar"
-                          name="file" required 
-                          />
-                      @endif
-                      <h3 class="mt-2">{{ (isset($employee)? $employee->name : '') }}</h3>
-                      <span class="badge rounded-pill bg-primary">{{ (isset($employee)? $employee->employee_id : '') }}</span>
-                      <span class="badge rounded-pill bg-primary">
-                          @if(optional($employee->getDepartment)->department != null)
-                              {{ $employee->getDepartment->department }}
-                          @endif
-                        </span>
-                    </div>
-                  </div>
-                  <hr class="my-0" />
-                <div class="card-body">
-                    <div class="row">
-                      <div class="mb-3 col-md-6">      
-                        <label for="employee_id" class="form-label">Employee-ID</label> <br />
-                        <span>{{ (isset($employee)? $employee->employee_id : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="name" class="form-label">Name</label> <br />
-                        <span>{{ (isset($employee)? $employee->name : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="email" class="form-label">E-mail</label> <br />
-                        <span>{{ (isset($employee)? $employee->email : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="nrc_number" class="form-label">NRC</label> <br />
-                        <span>{{ (isset($employee)? $employee->nrc_number : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label class="form-label" for="phone">Phone Number</label> <br />
-                        <span>{{ (isset($employee)? $employee->phone : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="address" class="form-label">Address</label> <br />
-                        <span>{{ (isset($employee)? $employee->address : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="birthday" class="form-label">Birthday</label> <br />
-                        <span>{{ (isset($employee) ? \Carbon\Carbon::parse($employee->birthday)->format('Y-m-d') : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="date_of_join" class="form-label">Date Of Join</label> <br />
-                        <span>{{ (isset($employee) ? \Carbon\Carbon::parse($employee->date_of_join)->format('Y-m-d') : '') }}</span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="department_id" class="form-label">Department</label> <br />
-                        <span>
-                          @if(optional($employee->getDepartment)->department != null)
-                              {{ $employee->getDepartment->department }}
-                          @endif
-                        </span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="gender" class="form-label">Gender</label> <br />
-                        <span>
-                          @if(isset($employee))
-                          {{ $employee->gender == 0 ? 'Male' : 'Female' }}
-                        @endif
-                        </span>
-                      </div>
-
-                      <div class="mb-3 col-md-6">
-                        <label for="status" class="form-label">Gender</label> <br />
-                        <span>
-                          @if(isset($employee))
-                          {{ $employee->status == 0 ? 'Leave' : 'Present' }}
-                        @endif
-                        </span>
+                 <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                      <div class="card-body d-flex justify-content-start">
+                          <div class="gap-4 ">
+                            @if(isset($employee))
+                                <img
+                                src="{{$employee->profilePath()}}"
+                                alt="user-avatar"
+                                class="rounded-circle border border-info p-1"
+                                height="140"
+                                width="140"
+                                id="uploadedAvatar"
+                                name="file" required 
+                                />
+                            @endif
+                            <p class="mt-2 fs-4 mx-2">{{ (isset($employee)? $employee->name : '') }}</p>
+                            <span>{{ (isset($employee)? $employee->employee_id : '') }}</span> | <span class="text-success fs-5">{{ (isset($employee)? $employee->phone : '') }}</span><br>
+                            <span class="badge rounded-pill bg-primary">
+                                @if(optional($employee->getDepartment)->department != null)
+                                    {{ $employee->getDepartment->department }}
+                                @endif
+                              </span>
+                          </div>
                       </div>
                     </div>
-                  </form>
-                </div>
-                <!-- /Account -->
+                    <div class="col-md-6 col-sm-12">
+                        <div class="p-3" style="border-left:2px dashed #ddd">
+                          <p><strong>Name : </strong>{{ (isset($employee)? $employee->email : '') }}</p>
+                          <p><Strong>NRC : </Strong>{{ (isset($employee)? $employee->nrc_number : '') }}</p>
+                          <p><strong>Address : </strong>{{ (isset($employee)? $employee->address : '') }}</p>
+                          <p><strong>Birthday : </strong>{{ (isset($employee) ? \Carbon\Carbon::parse($employee->birthday)->format('Y-m-d') : '') }}</p>
+                          <p><strong>Join Date : </strong>{{ (isset($employee) ? \Carbon\Carbon::parse($employee->date_of_join)->format('Y-m-d') : '') }}</p>
+                          <p><strong>Gender : </strong>@if(isset($employee))
+                              {{ $employee->gender == 0 ? 'Male' : 'Female' }}
+                            @endif
+                          </p>
+                          <p><strong>Status : </strong><span class="badge rounded-pill bg-primary">
+                            @if(isset($employee))
+                              {{ $employee->status == 0 ? 'Leave' : 'Present' }}
+                            @endif
+                          </span></p>
+                        </div>
+                    </div>
+                 </div>
               </div>
             </div>
           </div>
